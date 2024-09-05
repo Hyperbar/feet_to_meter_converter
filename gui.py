eae92a28274a85191de3915158fa25415dfe4a0c
hyperbar <szthyperbar@gmail.com>
2024-09-05 21:14:39 +0200

    Add theme, add exit button

--- gui.py
+++ gui.py
@@ -3,6 +3,10 @@ import FreeSimpleGUI as sg
 # Import the make_conversion function from the conversion module
 from conversion import make_conversion
 
+
+# Apply a theme
+sg.theme('Black')
+
 # Create GUI elements
 
 # Label and input for entering feet
@@ -18,16 +22,26 @@ convert_btn = sg.Button("Convert")
 # Text element to display the conversion result in meters
 result_in_meter = sg.Text("", key="output")
 
+# Button to exit and close the program
+button_exit = sg.Button("Exit", key="exit")
+
 # Create the main window with all GUI elements
 window = sg.Window(title="Convertor - Hyperbar",
                    layout=[[feet_label, feet_input],
                            [inches_label, inches_input],
-                           [convert_btn, result_in_meter]])
+                           [convert_btn, button_exit,result_in_meter]])
 
 # Main application loop
 while True:
     # Read events and values from the window
     event, value = window.read()
+
+    match event:
+        case "exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+
 
     # Convert the input values to float
     feet = float(value["feet"])
@@ -39,5 +53,5 @@ while True:
     # Update the output text element with the conversion result
     window["output"].update(value=f"{conversion} m", text_color="white")
 
-# Close the window when the loop exits
+    # Close the window when the loop exits
 window.close()
